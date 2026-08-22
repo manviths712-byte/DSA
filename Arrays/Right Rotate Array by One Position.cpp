e30a83c84e1697ff2fa7e8de898a935d2bd4c269
Manvith s <manviths712@gmail.com>
2026-08-22 20:16:56 +0530

    Right Rotate Array by One Position implementation

---
+++ Arrays/Right Rotate Array by One Position.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int arr[5] = {1, 2, 3, 4, 5};
+    int n = 5;
+
+    int last = arr[n - 1];
+
+    for(int i = n - 1; i > 0; i--){
+        arr[i] = arr[i - 1];
+    }
+
+    arr[0] = last;
+
+    cout << "Array after right rotation by one position: ";
+
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
