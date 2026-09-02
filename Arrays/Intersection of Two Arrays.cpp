ff9b59a4b63b86a919574b702dd3640806871883
Manvith s <manviths712@gmail.com>
2026-09-02 19:35:40 +0530

    program to find intersection of two arrays

---
+++ Arrays/Intersection of Two Arrays.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int arr1[5] = {1, 2, 3, 4, 5};
+    int arr2[5] = {3, 4, 5, 6, 7};
+
+    int n1 = 5;
+    int n2 = 5;
+
+    cout << "Intersection of two arrays: ";
+
+    for(int i = 0; i < n1; i++) {
+
+        for(int j = 0; j < n2; j++) {
+
+            if(arr1[i] == arr2[j]) {
+                cout << arr1[i] << " ";
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
